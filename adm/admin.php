8b23aa6879314e1dc59c954c4170957580cc364e
Will-Carlo <wil_fer_25@outlook.es>
2022-09-12 17:29:31 -0400

    corrigiendo accesos de admin y link de página principal a login

--- adm/admin.php
+++
@@ -1,6 +0,0 @@
-<?php include("template/header.php")?>
-    <!-- backend -->
-    <p>hola mundo</p> 
-
-
-<?php include("template/footer.php")?>
--- adm/template/header.php
+++ adm/template/header.php
@@ -10,7 +10,7 @@
     
     <?php $url="http://".$_SERVER['HTTP_HOST']."/Users/ASUS/Documents/umsa%20-%20informática/2022%20II/02.%20INF%20281/proyecto281/adm/sitioadmin"?>
    <div>
-    <a href="<?= $url?>/inicio.php">Inicio</a>
+    <a href="<?= $url?>/admin.php">Inicio</a>
     <a href="<?= $url?>/personal.php">Personal</a>
     <a href="<?= $url?>/clientes.php">Clientes</a>
     <a href="<?= $url?>/menu.php">Menú</a>
